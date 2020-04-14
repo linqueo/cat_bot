77b37d63a0e9292187a74a4a9847316ebf064f2d
unknown <www.neocurry@gmail.com>
2020-04-14 18:06:56 -0500

    First commit

---
+++ bot.js
@@ -0,0 +1,38 @@
+const eris = require('eris');
+const ErisJSON = require('eris-json');
+const config = require('./config.js');
+var frases = require('./quotesArray.json')
+
+const bot = new eris.Client(config.token);
+
+
+bot.on('ready', () => {
+   console.log('Conectado y listo.');
+});
+
+var randomQuote = frases[Math.floor(Math.random() * frases.length)];
+var display = JSON.stringify(randomQuote.quote);
+
+// Cada vez que se mande activador '!oracion' el bot respondera con un mensaje
+bot.on('messageCreate', (msg) => {
+  if (msg.content === 'Yo te invoco') {
+    var interval = setInterval(function(){
+        bot.createMessage(msg.channel.id, mensaje)
+    },  25200000);
+} else if  (msg.content === 'Maiq dime una frase') {
+  bot.createMessage(msg.channel.id, mensaje);
+}});
+
+var mensaje =  {embed: {
+    description: (display),
+    color: 0x674EA7,
+    title: `M'aiq dice:`
+  }}
+
+bot.on('error', err => {
+   console.warn(err);
+});
+
+bot.connect();
+
+
